99ad455785bf51f3117320168a69cec657634c4b
amoljangid11 <amoljangid33@gmail.com>
2020-06-05 09:09:42 +0530

    react native chat app

--- App.js
+++ App.js
@@ -1,96 +1,44 @@
-import React from 'react';  
-import {StyleSheet, Text, View, Image} from 'react-native';  
-import { createAppContainer } from 'react-navigation';  
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import Icon from 'react-native-vector-icons/Ionicons';  
-class HomeScreen extends React.Component {  
-  render() {  
-    return (  
-        <View style={styles.container}>  
-          <Text>Home Screen</Text>  
-        </View>  
-    );  
-  }  
-}  
-class SearchScreen extends React.Component {  
-  render() {  
-    return (  
-        <View style={styles.container}>  
-          <Text>Chat Screen</Text>  
-        </View>  
-    );  
-  }  
-}
-class BookServiceScreen extends React.Component{
-  render(){
-    return(
-      <View style = {styles.container}>
-        <Text>Book Service</Text>
-      </View>
-    )
-  }
-} 
+import React, {Component} from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {LoginScreen} from './src/LoginScreen';
+import {HomeScreen} from './src/HomeScreen';
+import {ChatScreen} from './src/ChatScreen';
+import {VideoPlayer} from './src/VideoPlayer';
+import {BlockedUsers} from './src/BlockedUsers';
+import {GroupChatScreen} from './src/GroupChatScreen';
+import NavigationService from './src/NavigationService';
+import {ImageViewer} from './src/ImageViewer';
+import {MainCallScreen} from './src/MainCallScreen';
+import {CallingScreen} from './src/CallingScreen';
 
-class ChatService extends React.Component{
-  render(){
-    return(
-      <View style = {styles.container}>
-        <Text>Chat Screen</Text>
-      </View>
-    )
+const navigator = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Home: HomeScreen,
+    Chat: ChatScreen,
+    Group: GroupChatScreen,
+    Video: VideoPlayer,
+    Block: BlockedUsers,
+    Image: ImageViewer,
+    MainCallScreen: MainCallScreen,
+    CallingScreen: CallingScreen,
+  },
+  {
+    initialRouteName: 'Login',
+  },
+);
+
+const AppContainer = createAppContainer(navigator);
+export default class App extends Component {
+  render() {
+    console.disableYellowBox = true;
+    return (
+      <AppContainer
+        ref={navigatorRef => {
+          NavigationService.setTopLevelNavigator(navigatorRef);
+        }}
+      />
+    );
   }
 }
-  
-const TabNavigator = createBottomTabNavigator(  
-    {  
-      Home:{  
-        screen:HomeScreen,  
-        navigationOptions:{  
-            
-          tabBarIcon:({tintColor})=>(  
-            <Image source = {require('./asset/Images/1.png')} style = {{width: 50, height: 50, tintColor: tintColor}}/>
-
-          )  
-        }  
-      },  
-      Search: {  
-        screen: SearchScreen,  
-        navigationOptions:{  
-            
-          tabBarIcon:({tintColor})=>(  
-              <Image source = {require('./asset/Images/3.png')} style={{width: 50, height: 50, tintColor: tintColor}}/>
-          )  
-        }  
-      },
-      BookService: {
-        screen: BookServiceScreen,
-        navigationOptions: {
-          
-          tabBarIcon:({tintColor})=>(
-            <Icon name="ios-book" color={tintColor} size={25}/>
-          )
-        }
-      },
-      Chat: {
-        screen: ChatService,
-        navigationOptions: {
-          
-          tabBarIcon: ({tintColor})=>(
-            <Image source = {require('./asset/Images/4.png')} style = {{width: 40, height: 40, tintColor: tintColor}}/>
-          )
-        }
-      }  
-    },  
-    {  
-      initialRouteName: "Home"  
-    },  
-);  
-const styles = StyleSheet.create({  
-  container: {  
-    flex: 1,  
-    justifyContent: 'center',  
-    alignItems: 'center'  
-  },  
-});  
-  
-export default createAppContainer(TabNavigator);  
